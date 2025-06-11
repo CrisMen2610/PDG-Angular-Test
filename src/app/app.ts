48564f41be159c32086b0da51eaf3c6165e8371b
CrisMen2610 <crisdamencast@gmail.com>
2025-06-11 17:56:30 -0500

    feat(test): Angular Initial Config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Card } from '../components/card/card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Card],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
