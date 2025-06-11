30e08b16dd74c6cf5d3a957beafbcc5553e87310
CrisMen2610 <crisdamencast@gmail.com>
2025-06-11 18:18:39 -0500

    feat(header-component): header created

--- src/components/card/card.component.ts
+++ src/components/card/card.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Header],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
